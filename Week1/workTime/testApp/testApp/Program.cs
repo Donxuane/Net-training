928caa8e27abe7225e8bffad03f92240dad13d26
vakho <vakho@LAPTOP-G2M8VSPR>
2026-02-06 19:22:08 +0300

    updated changes

--- Week1/workTime/testApp/testApp/Program.cs
+++ Week1/workTime/testApp/testApp/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string nameSurname = "Vakhtangi Chitashvili";
+short age = 21, height = 180;
+char symbols = 'a';
+Console.WriteLine($"name - {nameSurname}\n{age}\n{height}\n{symbols}");
