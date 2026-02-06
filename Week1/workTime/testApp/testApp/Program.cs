80e0cc2194007e2429584c77d26771c1e7dbd64e
vakho <vakho@LAPTOP-G2M8VSPR>
2026-02-06 21:48:56 +0300

    simple math operations

--- Week1/workTime/testApp/testApp/Program.cs
+++ Week1/workTime/testApp/testApp/Program.cs
@@ -1,4 +1,8 @@
-﻿string nameSurname = "Vakhtangi Chitashvili";
-short age = 21, height = 180;
-char symbols = 'a';
-Console.WriteLine($"name - {nameSurname}\n{age}\n{height}\n{symbols}");
+﻿int firstNumber = 1000;
+int secondNumber = 8;
+
+Console.WriteLine($"firstNumber - {firstNumber}\nsecondNumber - {secondNumber}");
+Console.WriteLine("Sum");
+int sum = firstNumber;
+Console.WriteLine(sum + secondNumber);
+Console.WriteLine(firstNumber);
