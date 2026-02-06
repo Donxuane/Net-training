e7699db758536d9cafdbcda9ab719b229ab33ab1
Home <Home@DESKTOP-1DI5E15>
2026-02-06 21:48:47 +0400

    cvlileba

--- Week1/workTime/mamuka/mamuka/Program.cs
+++ Week1/workTime/mamuka/mamuka/Program.cs
@@ -2,4 +2,6 @@
 string namesurname = "mamuka khucishvili";
 short age = 31, height = 178;
 char symbols = '7';
-Console.WriteLine($"name - {namesurname}\n{age}\n{symbols}");
+decimal mamuka = 777777;
+Console.WriteLine($"name - {namesurname}\n{age}\n{symbols}\n{mamuka}");
+
