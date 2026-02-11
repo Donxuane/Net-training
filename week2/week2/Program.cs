1e1f03393cb6d485d319922ebecc07ac093ced6d
khati <khati@KHATIA>
2026-02-11 21:15:28 +0400

    if else task

---
+++ week2/week2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.Design;
+
+string color = "red";
+if (color == "red")
+{
+    Console.WriteLine(color);
+
+}
+else if (color == "yellow")
+{
+    Console.WriteLine(color);
+}
+else if (color == "green")
+{
+    Console.WriteLine(color);
+}
+else
+{ Console.WriteLine("error"); }
+
+
+
+
+
+
+
+
