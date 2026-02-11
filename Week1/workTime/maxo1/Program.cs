e443c380587feb79a52b54561df9cebe812efcf9
Maxo <Maxo@DESKTOP-KR9H591>
2026-02-11 21:15:18 +0400

    task 3

--- Week1/workTime/maxo1/Program.cs
+++ Week1/workTime/maxo1/Program.cs
@@ -1,10 +1,31 @@
-﻿string name = "Maxo";
+﻿using System.ComponentModel.Design;
+using System.Xml.Schema;
+
+string name = "Maxo";
 string Surname = "Mgeladze";
 int applyamount = 15;
 double risekilogram = 3.5;
 bool canYouLift50kg = true;
 Console.WriteLine($"{name}\n{Surname}\n{applyamount}\n{risekilogram}\n{canYouLift50kg}");
+//task2 
+string colors = "yellow";
+if (colors == "Red")
+{
+    Console.WriteLine(colors);
 
+}
+else if (colors == "green")
+
+{
+    Console.WriteLine("green");
+ 
+
+} else
+{
+    Console.WriteLine("error");
+}
+
+    
 
 
 
