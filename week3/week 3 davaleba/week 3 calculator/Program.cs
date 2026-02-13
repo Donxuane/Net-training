c0c8d131e551861c9d8bd71cf8808193d55c7bf5
khati <khati@KHATIA>
2026-02-13 22:50:00 +0400

    even/odd task

---
+++ week3/week 3 davaleba/week 3 calculator/Program.cs
@@ -0,0 +1,47 @@
+﻿while (true)
+
+{
+    Console.Write("first number: ");
+    double firstNumber = Convert.ToInt32(Console.ReadLine());
+    Console.Write("mathoperator: ");
+    string mathOperator = Console.ReadLine();
+    Console.Write("second number: ");
+    double secondNumber = Convert.ToInt32(Console.ReadLine());
+   
+
+    if (mathOperator == "+")
+    {
+        Console.WriteLine(firstNumber + secondNumber);
+      
+    }
+    else if (mathOperator == "-")
+    {
+        Console.WriteLine(firstNumber - secondNumber);
+    }
+    else if (mathOperator == "*")
+    {
+        Console.WriteLine(firstNumber * secondNumber);
+    }
+    else if (mathOperator == "/")
+    {
+        Console.WriteLine(firstNumber / secondNumber);
+    }
+    else if (mathOperator == "sqrt")
+    {
+        Console.WriteLine(Math.Sqrt(firstNumber));
+        Console.WriteLine(Math.Sqrt(secondNumber));
+    }
+    else
+    {
+        Console.WriteLine("გამოიყენეთ მხოლოდ +, -, *, /, sqrt");
+
+    }
+    string appbreak = Console.ReadLine();
+
+    if (appbreak == "close")
+    {
+        break;
+    }
+
+
+}
---
+++ week3/week 3 davaleba/week 3 calculator/week 3, davaleba 2/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("This application checks if number is even or odd.\n");
+Console.Write("When you are asked for a number simply input any number you want.\n");
+Console.Write("If you want to exit the app, enter 0.\n");
+Console.WriteLine();
+Console.WriteLine();
+
+while (true)
+{
+
+    Console.WriteLine("Enter number: ");
+    int inputnumber = Convert.ToInt32(Console.ReadLine());
+    if (inputnumber == 0)
+    {
+        break;
+    }
+    if (inputnumber % 2 == 0)
+    {
+        Console.WriteLine("even\n");
+
+    }
+    else
+    {
+        Console.WriteLine("odd\n");
+        
+    }
+    
+}
