3c019c51b50e5b2917add3c396892b98dc00cd3f
khati <khati@KHATIA>
2026-02-14 02:18:37 +0400

    password task

---
+++ week3/Password/Program.cs
@@ -0,0 +1,26 @@
+﻿string password = "cat";
+int tries = 0;
+
+while (true)
+{
+    Console.WriteLine("enter password");
+    string userPassword = Console.ReadLine();
+    
+
+if (userPassword == password)
+{
+    Console.WriteLine("welcome");
+    break;
+
+}
+else
+{
+    Console.WriteLine("Wrong password");
+    tries++;
+} 
+if (tries == 3)
+    { Console.WriteLine("acces denied");
+        break;
+    }
+  
+}
