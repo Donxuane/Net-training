f739861008c23b827499f67e701ed78a6626952d
Maxo <Maxo@DESKTOP-KR9H591>
2026-02-14 17:57:59 +0400

    task3

--- Week1/workTime/maxo1/Program.cs
+++ Week1/workTime/maxo1/Program.cs
@@ -24,6 +24,7 @@ else if (colors == "green")
 {
     Console.WriteLine("error");
 }
+//
 
     
 
---
+++ week2 task 2/week2,task2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
+
+while (true)
+{
+    Console.WriteLine("Calculator");
+    Console.WriteLine("firs Number");
+    double firstNumber = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("operation");
+    string operation = Console.ReadLine();
+    Console.WriteLine("second Number");
+    double secondNumber = Convert.ToDouble(Console.ReadLine());
+    if (operation == "+")
+    {
+        Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
+    }
+    Console.Write("Continue Execution?if so press T,and close app press d or D");
+    string appbreak = Console.ReadLine();
+    if (appbreak == "d"
+            || appbreak == "D")
+    {
+        break;
+    }
+   else if (appbreak == "T")
+    {
+        continue;
+    }
+    else
+    {
+        break;
+    }
+}
+//
+while (true)
+{
+    Console.WriteLine("Calculator minus");
+    Console.WriteLine("firstNumber");
+    double firstNumber = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Operator");
+    string Operator = Console.ReadLine();
+    Console.WriteLine("secondNumber");
+    double secondNumber = Convert.ToDouble(Console.ReadLine());
+    if (Operator == "/")
+    {
+        Console.WriteLine($"{firstNumber} / {secondNumber} = {firstNumber} / {secondNumber}");
+    }
+    Console.WriteLine("continiue execution? if so press S, and to close app press space");
+    string appbreak = Console.ReadLine();
+    if (appbreak == " ")
+    {
+        break;
+    }
+    else if (appbreak == "S")
+    {
+        continue;
+    }
+    else
+    {
+        break;
+    }
+}
