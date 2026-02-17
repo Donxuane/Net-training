3f43d93b85618bebd310ca6ebfcc4c7f33d377e3
Maxo <Maxo@DESKTOP-KR9H591>
2026-02-17 21:33:26 +0400

    age

---
+++ maxo new/age/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("age");
+while (true)
+{
+    int age = Convert.ToInt32(Console.ReadLine());
+    if (age == -1)
+    {
+        break;
+    }
+    else if (age < 0)
+    {
+        Console.WriteLine("invalid age");
+    }
+    else if (age > 0 && age < 18)
+    {
+        Console.WriteLine("you are minor");
+    }
+    else if (age >= 18 && age < 65)
+    {
+        Console.WriteLine("you are adult");
+    }
+
+    else if (age >= 65)
+    {
+        Console.WriteLine("you are Senior");
+    }
+
+}
