09d5e98efa5c432a04cfb529d5369154ed2d1984
Maxo <Maxo@DESKTOP-KR9H591>
2026-02-21 23:18:03 +0400

    task words

---
+++ maxo new/TASK 1 NUMBERS/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+        int[] numbers = { 1, 2, 3, 4, 5 };
+
+        for (int i = numbers.Length - 1; i >= 0; i--)
+        {
+            Console.Write(numbers[i]);
+            if (i > 0)
+                Console.Write(", ");
+        }
+        Console.WriteLine("]");
+//
+int[] numbers2 = { 1, 2, 3, 4, 5 };
+for (int i = 0; i < numbers2.Length; i++)
+{
+    Console.Write(numbers2[i]);
+    if (i < numbers2.Length - 1)
+        Console.Write(", ");
+}
+Console.WriteLine("]");
---
+++ maxo new/words/words/Program.cs
@@ -0,0 +1,9 @@
+﻿string[] words = { "cat", "dog", "code" };
+foreach(string word in words)
+{
+    for(int i =word.Length-1; i>=0; i--)
+    {
+        Console.Write(word[i]);
+    }
+    Console.WriteLine();
+}
