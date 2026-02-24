523aec6608d1a0291dc095f0ae5d10f01e1257af
vakht <vakht@DESKTOP-H9JUF6E>
2026-02-24 21:19:32 +0400

    classes

---
+++ Week4-3/LuckyWheel2/LuckyWheel2/Person.cs
@@ -0,0 +1,20 @@
+﻿namespace LuckyWheel2;
+
+public class Person
+{
+    public char EnterLetter()
+    {
+        string input = Console.ReadLine();
+        char validChar = ValidateInputChar(input);
+        return validChar;
+    }
+
+    private char ValidateInputChar(string input)
+    {
+        if(input.Length > 1)
+        {
+            return '0';
+        }
+        return char.Parse(input);
+    }
+}
