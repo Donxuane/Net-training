98a678efb88c3169d31d1a8c8e39f4efba656ee5
khati <khati@KHATIA>
2026-03-03 20:46:55 +0400

    Person task

---
+++ week 5/person/Program.cs
@@ -0,0 +1,14 @@
+﻿//უნდა შევქმნა person ტიპის ობიექტი, რომელსაც გავუწერ მახასიათებლებს და განვუსაზღვრაც 2 მეთოდს:
+//პირველი მეთოდი დაბეჭდავს პერსონაზე ინფორმაციას, მეროე დააბრუნებს ასაკს.
+//person ტიპის ობიექტს კონსტრუქტორიდან ვანიჭებთ მნიშვნელობებს.
+
+using person;
+
+Person InfoToka = new Person("Tornike", "Beradze", 35, "tokatoka12");
+InfoToka.PrintInfo();
+int tokaAge = InfoToka.GetAge();
+Console.WriteLine(tokaAge);
+InfoToka.GetEmail("Tornike");
+string email = InfoToka.GetEmail("Vakho");
+Console.WriteLine(email);
+InfoToka.profession("veterinarian");
---
+++ week 5/person/person.cs
@@ -0,0 +1,41 @@
+﻿
+namespace person
+{
+    internal class Person
+    {
+        private string Name { get; set; }
+        private string SurName { get; set; }
+        private int Age { get; set; }
+        private string Email { get; set; }
+
+        public Person(string name, string surName, int age, string email)
+        {
+            Name = name;
+            SurName = surName;
+            Age = age;
+            Email = email;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"{Name} {SurName} {Age} {Email}");
+
+        }
+
+        public int GetAge()
+        { return Age; }
+
+        public string GetEmail(string name)
+        {
+            if (Name == name)
+            { return Email; }
+            else { return "invalid"; }
+
+
+        }
+
+        public void profession(string vet)
+        { Console.WriteLine(vet); }
+    }
+}
+
