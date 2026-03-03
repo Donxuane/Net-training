f47a510c3d6ca4f8b1d3e8a8c8f422b5899a0597
khati <khati@KHATIA>
2026-03-03 21:15:06 +0400

    Student task

---
+++ week 5/Student task/Program.cs
@@ -0,0 +1,8 @@
+﻿
+using Student_task;
+
+Student info = new Student();
+info.information("khatia", "motskobili", 3.31);
+info.PrintInfo();
+double gpa = info.GetGpa();
+Console.WriteLine(gpa);
---
+++ week 5/Student task/student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_task;
+
+internal class Student
+{
+    private string Name;
+    private string SurName;
+    private double Gpa;
+
+    public void information(string name,string surName, double gpa)
+    {
+        Name = name;
+        SurName = surName;
+        Gpa = gpa;
+    
+    }
+
+    public void PrintInfo()
+    {
+        Console.WriteLine(Name + SurName + Gpa);
+    }
+
+    public double GetGpa()
+    { return  Gpa; }
+        
+}
+
