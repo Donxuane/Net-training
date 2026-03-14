3a271d97994aecc0971e869c5112656971ebfa4d
vakht <vakht@DESKTOP-H9JUF6E>
2026-03-14 20:45:38 +0400

    added

---
+++ Week6/ReadWriteIntoFile/ReadWriteIntoFile/Market.cs
@@ -0,0 +1,52 @@
+﻿namespace ReadWriteIntoFile;
+
+public class Market
+{
+
+    private string _path = Path.GetFullPath(
+     Path.Combine(AppContext.BaseDirectory, @"..\..\..\data.txt"));
+
+
+    public bool Login(string userName, string password)
+    {
+        string path = 
+                Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\login.txt");
+
+        using(StreamReader reader = new(path))
+        {
+            while (true)
+            {
+                try
+                {
+                    string line = reader.ReadLine();
+                    if (line == null) break;
+
+                    if (line.Contains(userName))
+                    {
+                        string[] data = line.Split(' ');
+                        if (data[1] == password)
+                        {
+                            return true;
+                        }
+                    }
+                    
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public void PrintProducts()
+    {
+        using (StreamReader reader = new(_path))
+        {
+            string data = reader.ReadToEnd();
+            Console.WriteLine(data);
+        }
+    }
+}
---
+++ Week6/ReadWriteIntoFile/ReadWriteIntoFile/Program.cs
@@ -0,0 +1,10 @@
+﻿using ReadWriteIntoFile;
+
+Market market = new();
+string userName = Console.ReadLine();
+string password = Console.ReadLine();
+bool checkout = market.Login(userName,password);
+if (checkout)
+{
+    market.PrintProducts();
+}
