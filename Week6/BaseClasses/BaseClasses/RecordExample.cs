c8ed5159bc5d5def99f799c65146b7f8dc179a25
vakht <vakht@DESKTOP-H9JUF6E>
2026-03-31 20:23:42 +0400

    test

--- Week6/BaseClasses/BaseClasses/RecordExample.cs
+++ Week6/BaseClasses/BaseClasses/RecordExample.cs
@@ -1,1 +1,46 @@
-﻿
+﻿namespace BaseClasses;
+public class InventoryItem
+{
+
+    public double X {  get;  }
+    public InventoryItem(double x)
+    {
+        X = x;
+        
+    }
+
+    //accessible for derived class to be extended
+    public virtual void Print()
+    {
+        Console.WriteLine("test");
+    }
+
+}
+
+
+public  class Food : InventoryItem
+{
+    public double X;
+    public Food(double x):base(x)
+    {
+        X = x;
+    }
+    //overriding
+    public override void Print()
+    {
+        Console.WriteLine(X);
+    }
+
+    //overloading
+    public void Print(string value)
+    {
+        Console.WriteLine($"{value} {X}");
+    }
+
+    public int Print(int value, string text)
+    {
+        Console.WriteLine(text);
+        return value;
+    }
+
+}
