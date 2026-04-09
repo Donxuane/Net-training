fbeb85ec529ddcf5bf1ba5cc39d9ed06d4b9189f
vakht <vakht@DESKTOP-H9JUF6E>
2026-04-09 17:11:19 +0400

    test

---
+++ CRUD/FirstCrud/FirstCrud/Crud.cs
@@ -0,0 +1,28 @@
+﻿using Dapper;
+using Npgsql;
+
+namespace FirstCrud;
+
+public class Crud
+{
+    public string ConnectionString = "Host=localhost;Port=5432;Database=khatiafirsttest;Username=postgres;Password=;";
+    public void InsertData(Test test)
+    {
+        var command = @"insert into test (id,name,surname,isadult )
+                            values(@Id,@Name,@Surname,@IsAdult);";
+        using (NpgsqlConnection connection = new NpgsqlConnection(ConnectionString))
+        {
+            connection.Open();
+            connection.Execute(command, test);
+        }
+    }
+
+    public List<Test> GetData()
+    {
+        string query = "select * from test;"; 
+        using var connection = new NpgsqlConnection(ConnectionString);
+        connection.Open();
+        return (connection.Query<Test>(query)).ToList();
+        
+    }
+}
---
+++ CRUD/FirstCrud/FirstCrud/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FirstCrud;
+
+Console.WriteLine("Hello, World!");
+
+
+Test test = new Test { Id = 5, IsAdult = true ,Name = "Tornike", SurName = "Beradze"};
+
+Crud crud = new Crud();
+
+List<Test> collection = crud.GetData();
+
+foreach(Test col in collection)
+{
+    Console.WriteLine($"{col.Id}, {col.Name}, {col.SurName}, {col.IsAdult}");
+}
