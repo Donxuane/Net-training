f2bb611e09a63cb67c0f205e9b672e5371593b34
vakht <vakht@DESKTOP-H9JUF6E>
2026-04-21 19:38:53 +0400

    test

---
+++ Week9/Generics/Generics/Generics12.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace Generics;
+
+public class Generics12
+{
+    public void PrintInfo<T>(T parameter) 
+    {
+        if(parameter is int test)
+        {
+            Console.WriteLine($"parameter is int value {test}");
+        }
+
+        if(parameter is string testString)
+        {
+            Console.WriteLine($"parameter is int value {testString}");
+        }
+
+        var type = parameter.GetType();
+
+        if(type.GetProperties().Length != 0)
+        {
+            Console.WriteLine(JsonSerializer.Serialize(type.GetProperties().Select(x=>x.Name)));
+        }
+    }
+
+    public void UpdateTabele<T>(T parameter) where T : class
+    {
+        var command = @"update testclass t set name = @Name, description = @Description where t.id = @Id";
+        StringBuilder stringBuilder = new StringBuilder();
+        Dictionary<string, object> parameteres = new Dictionary<string, object>();
+        stringBuilder.Append($"update {nameof(parameter).ToLower()} set ");
+
+        var type = parameter.GetType();
+        var properties = type.GetProperties();
+
+        foreach( var property in properties )
+        {
+            if (property.Name != "Id")
+            {
+                stringBuilder.Append($"{property.Name.ToLower()} = @{property.Name},");
+            }
+            //var proValue = property.GetValue(parameter);
+            parameteres.Add(property.Name, null);
+        }
+
+        stringBuilder.Append(@"where id = @Id");
+
+        var query = stringBuilder.ToString();
+    }
+}
---
+++ Week9/Generics/Generics/Program.cs
@@ -0,0 +1,20 @@
+﻿using Generics;
+
+Generics12 generics = new Generics12();
+
+generics.PrintInfo<int>(12);
+
+generics.PrintInfo<TestClass>(new TestClass
+{
+    Description = "fdsdsd",
+    Id = 1,
+    Name = "Test"
+});
+
+
+generics.UpdateTabele(new TestClass
+{
+    Description = "fdsdsd",
+    Id = 1,
+    Name = "Test"
+});
